8cd5fcb01ee8e3192734e6eb4ea0404a44703fb4
Maria Trujillo <mptruj015@gmail.com>
2020-04-13 22:07:51 +0200

    set initial state and reducer

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,14 @@
+import React, { useState, useReducer } from 'react';
+
+const initialState = {
+    item: 'learn abnout reducers',
+    completed: false,
+    id: 3892987589
+}
+
+const reducer = (state, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
